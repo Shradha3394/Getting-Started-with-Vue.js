caba589b520911ccaf3d9bdf2133d2e799c9642c
Shradha <sjoshi@ex2india.com>
2018-12-15 02:37:12 +0530

    Getting Started with Vue.js

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,93 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <h4 class="bg-primary text-white text-center p-2">
+            {{name}}'s To Do List
+        </h4>
+        <div class="container-fluid p-4">
+            <div class="row" v-if="filteredTasks.length == 0">
+                <div class="col text-center">
+                    <b>Nothing to do. Hurrah!</b>
+                </div>
+            </div>
+            <template v-else>
+                <div class="row">
+                    <div class="col font-weight-bold">Task</div>
+                    <div class="col-2 font-weight-bold">Done</div>
+                </div>
+                <div class="row" v-for="task in filteredTasks" :key="task.action">
+
+                    <div class="col">{{task.action}}</div>
+                    <div class="col-2">
+                        <input type="checkbox" v-model="task.done" class="form-check-input" />
+                        {{task.done}}
+                    </div>
+                </div>
+                <div class="row py-2">
+                    <div class="col">
+                        <input v-model="newItemText" class="form-control" />
+                    </div>
+                    <div class="col-2">
+                        <button class="btn btn-primary" v-on:click="addNewTodo" :disabled="newItemText === ''">Add</button>
+                    </div>
+                </div>
+                <div class="row bg-secondary py-2 mt-2 text-white">
+                    <div class="col text-center">
+                        <input type="checkbox" v-model="hideCompleted" class="form-check-input" />
+                        <label class="form-check-label font-weight-bold">
+                            Hide completed tasks
+                        </label>
+                    </div>
+                    <div class="col text-center">
+                        <button class="btn btn-sm btn-warning"
+                                v-on:click="deleteCompleted">
+                            Delete Completed
+                        </button>
+                    </div>
+                </div>
+
+            </template>
+        </div>
+    </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    export default {
+        name: 'app',
+        data() {
+            return {
+                name: "Shradha",
+                tasks: [],
+                hideCompleted: false,
+                newItemText: ""
+            }
+        },
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+        computed: {
+            filteredTasks() {
+                return this.hideCompleted ? this.tasks.filter(t => !t.done) : this.tasks;
+            }
+        },
+
+        methods: {
+            addNewTodo() {
+                var newTask = { action: this.newItemText, done: false };
+                this.tasks.push(newTask);
+                this.storeData();
+                this.newItemText = ""
+            },
+            storeData() {
+                localStorage.setItem("todos", JSON.stringify(this.tasks));
+            },
+            deleteCompleted() {
+                this.tasks = this.tasks.filter(t => !t.done);
+                this.storeData();
+            }
+        },
+
+        created() {
+            let data = localStorage.getItem("todos");
+            if (data != null) {
+                this.tasks = JSON.parse(data);
+            }
+        }
+    }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
