361d6c94cf2478e9a95f203378a12b3dad1cdb13
Shradha <sjoshi@ex2india.com>
2021-01-21 10:52:36 +0530

    api integration

--- src/App.vue
+++ src/App.vue
@@ -1,105 +1,141 @@
 <template>
-    <div id="app">
-        <h4 class="bg-primary text-white text-center p-2">
-            {{name}}'s To Do List
-        </h4>
-        <div class="container-fluid p-4">
-            
-            <template>
-                <div class="row py-2">
-                    <div class="col-4">
-                        <input v-model="searchItemText" placeholder="Search a Task" class="form-control" />
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col font-weight-bold">Task</div>
-                    <div class="col-2 font-weight-bold">Done</div>
-                </div>
-                <div class="row" v-for="task in filteredTasks" :key="task.action">
-
-                    <div class="col">{{task.action}}</div>
-                    <div class="col-2">
-                        <input type="checkbox" v-model="task.done" class="form-check-input" />
-                        {{task.done}}
-                    </div>
-                </div>
-                <div class="row" v-if="filteredTasks.length == 0">
-                    <div class="col text-center">
-                        <b>Nothing to do. Hurrah!</b>
-                    </div>
-                </div>
-                <div class="row py-2">
-                    <div class="col">
-                        <input v-model="newItemText" placeholder="Add new Task" class="form-control" />
-                    </div>
-                    <div class="col-2">
-                        <button class="btn btn-primary" v-on:click="addNewTodo" :disabled="newItemText === ''">Add</button>
-                    </div>
-                </div>
-                <div class="row bg-secondary py-2 mt-2 text-white">
-                    <div class="col text-center">
-                        <input type="checkbox" v-model="hideCompleted" class="form-check-input" />
-                        <label class="form-check-label font-weight-bold">
-                            Hide completed tasks
-                        </label>
-                    </div>
-                    <div class="col text-center">
-                        <button class="btn btn-sm btn-warning"
-                                v-on:click="deleteCompleted">
-                            Delete Completed
-                        </button>
-                    </div>
-                </div>
-
-            </template>
+  <div id="app">
+    <h4 class="bg-primary text-white text-center p-2">
+      {{ name }}'s To Do List
+    </h4>
+    <div class="container-fluid p-4">
+      <template>
+        <div class="row py-2">
+          <div class="col-4">
+            <input
+              v-model="searchItemText"
+              placeholder="Search a Task"
+              class="form-control"
+            />
+          </div>
         </div>
+        <div class="row">
+          <div class="col font-weight-bold">Task</div>
+          <div class="col-2 font-weight-bold">Done</div>
+        </div>
+        <div class="row" v-for="task in filteredTasks" :key="task.id">
+          <div class="col">{{ task.content }}</div>
+          <div class="col-2">
+            <input
+              type="checkbox"
+              v-model="task.done"
+              class="form-check-input" style="margin-left: 0.75rem;"
+              @change="markAsCompleted(task)"
+            />
+          </div>
+        </div>
+        <div class="row" v-if="filteredTasks.length == 0">
+          <div class="col text-center">
+            <b>Nothing to do. Hurrah!</b>
+          </div>
+        </div>
+        <div class="row py-2">
+          <div class="col">
+            <input
+              v-model="newItemText"
+              placeholder="Add new Task"
+              class="form-control"
+            />
+          </div>
+          <div class="col-2">
+            <button
+              class="btn btn-primary"
+              @click="addNewTodo"
+              :disabled="newItemText === ''"
+            >
+              Add
+            </button>
+          </div>
+        </div>
+        <div class="row bg-secondary py-2 mt-2 text-white">
+          <div class="col text-center">
+            <input
+              type="checkbox"
+              v-model="hideCompleted"
+              class="form-check-input" 
+            />
+            <label class="form-check-label font-weight-bold">
+              Hide completed tasks
+            </label>
+          </div>
+          <div class="col text-center">
+            <button class="btn btn-sm btn-warning" v-on:click="deleteCompleted" :disabled="disableCompleted">
+              Delete Completed
+            </button>
+          </div>
+        </div>
+      </template>
     </div>
+  </div>
 </template>
 <script>
-    export default {
-        name: 'app',
-        data: () => ({
-            name: "Shradha",
-            tasks: [],
-            hideCompleted: false,
-            newItemText: "",
-            searchItemText: ""
-        }),
+import axios from "axios";
+const API_URL = "http://localhost:3000/notes";
 
-        computed: {
-            filteredTasks() {
-                if (this.searchItemText !== '' && this.hideCompleted)
-                    return this.tasks.filter(t => t.action.toLowerCase().includes(this.searchItemText.toLowerCase()) && !t.done)
-                else if (this.searchItemText !== '')
-                    return this.tasks.filter(t => t.action.toLowerCase().includes(this.searchItemText.toLowerCase()));
-                else if (this.hideCompleted)
-                    return this.tasks.filter(t => !t.done);
-                else
-                    return this.tasks;
-            }
-        },
+export default {
+  name: "app",
+  data: () => ({
+    name: "Shradha",
+    tasks: [],
+    hideCompleted: false,
+    newItemText: "",
+    searchItemText: "",
+  }),
 
-        methods: {
-            addNewTodo() {
-                var newTask = { action: this.newItemText, done: false };
-                this.tasks.push(newTask);
-                this.storeData();
-                this.newItemText = ""
-            },
-            storeData() {
-                localStorage.setItem("todos", JSON.stringify(this.tasks));
-            },
-            deleteCompleted() {
-                this.tasks = this.tasks.filter(t => !t.done);
-                this.storeData();
-            }
-        },
+  computed: {
+    disableCompleted(){
+     return !this.tasks.find(t => t.done);
+    },
+    filteredTasks() {
+      if (this.searchItemText !== "" && this.hideCompleted)
+        return this.tasks.filter(
+          (t) =>
+            t.action
+              .toLowerCase()
+              .includes(this.searchItemText.toLowerCase()) && !t.done
+        );
+      else if (this.searchItemText !== "")
+        return this.tasks.filter((t) =>
+          t.action.toLowerCase().includes(this.searchItemText.toLowerCase())
+        );
+      else if (this.hideCompleted) return this.tasks.filter((t) => !t.done);
+      else return this.tasks;
+    },
+  },
 
-        created() {
-            let data = localStorage.getItem("todos");
-            if (data != null) {
-                this.tasks = JSON.parse(data);
-            }
-        }
-    }
+  methods: {
+    addNewTodo() {
+      var newTask = { content: this.newItemText, done: false };
+
+      axios.post(API_URL, newTask).then((res) => {
+        this.tasks.push(res.data);
+        this.newItemText = "";
+      });
+    },
+
+    deleteCompleted() {
+      var ids = this.tasks.filter((t) => t.done).map((t) => t._id);
+      axios
+        .post(`${API_URL}/delete`, { ids: ids })
+        .then((res) => (this.tasks = this.tasks.filter((t) => !t.done)));
+    },
+    
+    markAsCompleted(task) {
+      axios.put(API_URL, task);
+    },
+  },
+
+  created() {
+    axios.get(API_URL).then((res) => {
+      if (res.data != null) {
+        this.tasks = res.data;
+      }
+    });
+  },
+};
 </script>
